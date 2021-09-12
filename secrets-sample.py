14552a2076b1b0503c275aee28a9f18ba54b89d7
DougWilkinson <34913281+DougWilkinson@users.noreply.github.com>
2021-09-12 13:46:30 -0400

    added secrets-sample

---
+++ secrets-sample.py
@@ -0,0 +1,11 @@
+# secrets.py
+
+def version():
+    return "vv"
+
+mqttuser = "username"
+mqttpass = "pwd"
+mqttserver = "192.168.x.x"
+custom_topics = ("global/set","global/heartbeat")
+hasstopic = "homeassistant"
+code_url = "http://nn.nn.nn.nn:80/local/code/"
